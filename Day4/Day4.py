6c0602cce5f38ebdf5a8864175703aaf60a95839
Jortb <Jortbaarsma@gmail.com>
2023-01-12 00:07:42 +0100

    Completed Day4

--- Day4/Day4.py
+++ Day4/Day4.py
@@ -4,6 +4,7 @@ import math
 from enum import Enum
 import os
 import copy
+import re
 
 def load_input_lines(filename):
     file = open(filename, 'r')
@@ -15,10 +16,37 @@ def load_input_string(filename):
     data = file.read()
     return data
 
-class Rucksack:
-    def __init__(self,contents_string,nr_compartments=2):
-        self.contents_string = contents_string
-        self.len_contents = len(contents_string)
+class Range:
+    def __init__(self,start,stop):
+        self.start = start
+        self.stop = stop
+        self.bitmask = 0
+        for i in range(start,stop+1):
+            self.bitmask += 2**i
+        
+
+class RangeCompare:
+    def __init__(self,Range1 , Range2):
+        self.r1=Range1
+        self.r2=Range2
+        min_start = min([Range1.start,Range2.start])
+        min_stop = min([Range1.stop,Range2.stop])
+        max_start = max([Range1.start,Range2.start])
+        max_stop = max([Range1.stop,Range2.stop])
+
+        if Range1.stop == min_stop and Range1.start == max_start:
+            self.full_contained = True
+        elif Range2.stop == min_stop and Range2.start == max_start:
+            self.full_contained = True
+        else:
+            self.full_contained = False
+        # Task 2
+        self.overlap =(self.r1.bitmask & self.r2.bitmask)
+
+
+def Compare_ranges(Range1, Range2):
+
+    return
         
         
 
@@ -30,7 +58,7 @@ test_string = """2-4,6-8
 6-6,4-6
 2-6,4-8"""
 
-test_data = True
+test_data = False
 second_strategy = True
 
 if __name__ == "__main__":
@@ -39,7 +67,29 @@ if __name__ == "__main__":
     file_path = os.path.join(cur_dir,filename)
     data = load_input_string(file_path)
     if test_data: data = test_string
-    rucksack_list = data.split('\n')
+    lines_list = data.split('\n')
+    compares = []
+    fully_contained = 0
+    overlap_list = []
+    overlapping = 0
+    for line in lines_list:
+        b = re.compile(r"(\d+)-(\d+),(\d+)-(\d+)")
+        result = re.match(b, line)
+        if result is not None:
+            g0 = result.group(0)
+            g1 = result.group(1)
+            g2 = result.group(2)
+            g3 = result.group(3)
+            g4 = result.group(4)
+            r1 = Range(int(g1),int(g2))
+            r2 = Range(int(g3),int(g4))
+            c = RangeCompare(r1,r2)
+            if c.full_contained:
+                fully_contained += 1
+            if c.overlap:
+                overlapping += 1
+            overlap_list.append(c.overlap)
+            compares.append(c)
     points_task1= []
     points_task2= []
     rucksacks = []
@@ -47,8 +97,8 @@ if __name__ == "__main__":
     sum_task1 = sum(points_task1)
     sum_task2 = sum(points_task2)
 
-print("Total sum Task1 : "+str(sum_task1))
+print("Total sum Task1 : "+str(fully_contained))
 
 
-print("Total sum Task2 : "+str(sum_task2))
+print("Total sum Task2 : "+str(overlapping))
 input("Please press any key")
