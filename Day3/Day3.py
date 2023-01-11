a5cb88b21bdff2e08856b828d272167d5cd43ea2
Jortb <Jortbaarsma@gmail.com>
2023-01-11 23:18:15 +0100

    Working (ugly) version of Day3

--- Day3/Day3.py
+++ Day3/Day3.py
@@ -21,13 +21,22 @@ class Rucksack:
         self.len_contents = len(contents_string)
         self.__content_chars_to_numbers__()
 
+        self.shared_values = dict()
+        self.unique_values = dict()
+        self.my_dict_list = [dict()]*nr_compartments
         len_compartment = math.floor(self.len_contents / nr_compartments)
         self.compartment = [[0]]*nr_compartments
         self.compartment_string = [[""]]*nr_compartments
         for i in range(nr_compartments):
             self.compartment[i]         = self.contents[i*len_compartment:(i+1)*len_compartment]
             self.compartment_string[i]  = self.contents_string[i*len_compartment:(i+1)*len_compartment]
-            
+            self.my_dict_list[i] = self.__count_contents__(self.compartment[i])
+        self.my_dict = self.__count_contents__(self.contents)
+        for val in self.my_dict_list[0]:
+            if val in self.my_dict_list[1]:
+                self.shared_values[val] = 1
+            else:
+                self.unique_values[val] = 1
     def __content_chars_to_numbers__(self):
         self.contents = [0]*self.len_contents
         for i in range(self.len_contents):
@@ -39,6 +48,16 @@ class Rucksack:
                 value = 27 + ascii - 65 # A = 65
             self.contents[i] =value
 
+    def __count_contents__(self,content):
+        my_dict = dict()
+        for val in content:
+            if val in my_dict:
+                my_dict[val]+= 1
+            else:
+                my_dict[val] = 1
+        return my_dict
+        
+
 
 test_string = """vJrwpWtwJgWrhcsFMMfFFhFp
 jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
@@ -47,7 +66,7 @@ wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
 ttgJtRGJQctTZtZT
 CrZsJsPPZsGzwwsLwLmpwMDw"""
 
-test_data = True
+test_data = False
 second_strategy = True
 
 if __name__ == "__main__":
@@ -58,13 +77,40 @@ if __name__ == "__main__":
     if test_data: data = test_string
     rucksack_list = data.split('\n')
     points= []
+    points_task2= []
+    rucksacks = []
     for rucksack_string in rucksack_list:
         rucksack = Rucksack(rucksack_string)
+        for value in rucksack.shared_values.keys():
+            points.append(int(value))
         print("B")
+        rucksacks.append(rucksack)
         
 
-    sum = sum(points)
+    nr_groups = math.floor(len(rucksacks)/3.0)
+    
+    for i in range(nr_groups):
+        shared_items = dict()
+        group_rucksacks = rucksacks[i*3:(i+1)*3]
+        for rs in group_rucksacks:
+            for item in rs.my_dict.keys():
+                if item in shared_items:
+                    shared_items[item]+= 1
+                else:
+                    shared_items[item] = 1
+        for item in shared_items.keys():
+            if shared_items[item] > 2:
+                print("Shared item is "+ str(item))
+                points_task2.append(int(item))
+            else:
+                pass
+            
+    sum_task1 = sum(points)
+
+    sum_task2 = sum(points_task2)
+
+print("Total sum Task1 : "+str(sum_task1))
 
 
-print("Total sum : "+str(sum))
+print("Total sum Task2 : "+str(sum_task2))
 input("Please press any key")
